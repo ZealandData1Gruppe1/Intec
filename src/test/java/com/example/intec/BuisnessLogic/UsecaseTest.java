f11600da4cb22167528b0366ac2f972f64674a74
Jakob F <Jakobfoersom88@gmail.com>
2023-05-11 13:37:03 +0200

    Lavet Junit test
    Vi har skrevet Junit tests på:
    RegistrerPerson
    hentTransportFirma
    hentTransportFirmaer
    findesPerson
    findesTransportCompany
    findesOtherCompany
    
    Co-Authored-By: KevinDMort <112697725+KevinDMort@users.noreply.github.com>
    Co-Authored-By: SercanAtlantis <120471786+SercanAtlantis@users.noreply.github.com>
    Co-Authored-By: emil51z8 <120008103+emil51z8@users.noreply.github.com>

--- src/test/java/com/example/intec/BuisnessLogic/UsecaseTest.java
+++ src/test/java/com/example/intec/BuisnessLogic/UsecaseTest.java
@@ -2,22 +2,69 @@ package com.example.intec.BuisnessLogic;
 
 import com.example.intec.DBController.DataController;
 import com.example.intec.Entititer.Firma;
+import com.example.intec.Entititer.Person;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class UsecaseTest {
  Usecase uc = new Usecase();
     @Test
-    void tjekOmViKanHenteTransportFirmaFraDatabasen() {
+    void tjekAtViKanHenteTransportFirmaFraDatabasen() {
         Firma result = uc.hentTransportFirma("POSTNORD");
         assertEquals("POSTNORD",result.getFirmanavn());
     }
     @Test
-    void tjekOmDenIkkeReturnereNogetNårDenIkkeFindesIDatabasen(){
+    void tjekAtDenIkkeReturnereNogetNårDenIkkeFindesIDatabasen(){
         Firma result = uc.hentTransportFirma("Test");
         assertEquals(null,result.getFirmanavn());
+    }
+
+    @Test
+    void tjekAtSidsteFirmaIArraylistenErOTHER() {
+        ArrayList<Firma> result = uc.hentTransportFirmaer();
+        assertEquals(result.get(result.size()-1).getFirmanavn(),"OTHER");
+    }
+    @Test
+    void tjekAtSidsteFirmaIArraylistenErIkkeOTHER(){
+        ArrayList<Firma> result = uc.hentTransportFirmaer();
+        assertNotEquals(result.get(result.size()-1).getFirmanavn(),"POSTNORD");
+    }
+
+    @Test
+    void TjekkerAtFunktionFindesPersonKanIdentifisereOmEnPersonFindesIDatabsenEllerEj() {
+        Boolean result = uc.findesPerson(2323);
+        assertEquals(true,result);
+    }
+    @Test
+    void tjekkerAtFunktionFindesPersonIkkeReturnereTrueNårPersonenIkkeFindes(){
+        Boolean result = uc.findesPerson(0);
+        assertEquals(false,result);
+    }
+
+
+    @Test
+    void TjekkerAtFunktionFindesTransportCompanyKanIdentifisereOmEtTransportCompanyFindesIDatabsenEllerEj() {
+        Boolean result = uc.findesTransportCompany("POSTNORD");
+        assertEquals(true,result);
+    }
+    @Test
+    void tjekkerAtFunktionFindesTranportCompanyIkkeReturnereTrueNårTransporCompanyIkkeFindes(){
+        Boolean result = uc.findesTransportCompany("Test");
+        assertEquals(false,result);
+    }
+    @Test
+    void TjekkerAtFunktionFindesOtherCompanyKanIdentifisereOmEtOtherCompanyFindesIDatabsenEllerEj() {
+        Boolean result = uc.findesOtherCompany("Zealand");
+        assertEquals(true,result);
+    }
+    @Test
+    void tjekkerAtFunktionFindesOtherCompanyIkkeReturnereTrueNårOtherCompanyIkkeFindes(){
+        Boolean result = uc.findesOtherCompany("Test");
+        assertEquals(false,result);
     }
 
 }
